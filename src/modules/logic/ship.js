2695e43e63c62c2d041bc8746e9b2143702fa8be
Andrea2 <andreab88@hotmail.it>
2024-11-26 17:23:07 +0100

    fatto barchette

--- src/modules/logic/ship.js
+++ src/modules/logic/ship.js
@@ -1,11 +1,21 @@
 class Ship {
     #length;
+    #hitsTaken = 0;
+
     constructor(length) {
         this.#length = length;
     }
 
     get length() {
         return this.#length;
+    }
+
+    hit() {
+        this.#hitsTaken++;
+    }
+
+    isSunk() {
+        return this.#hitsTaken >= this.#length;
     }
 }
 
--- test/logic/ship.test.js
+++ test/logic/ship.test.js
@@ -9,4 +9,16 @@ describe("Ship", () => {
         const ship = new Ship(3);
         expect(ship.length).toBe(3);
     })
+
+    test("is not sunk", () => {
+        const ship = new Ship(3);
+        expect(ship.isSunk()).toBe(false);
+    })
+
+    test("is sunk", () => {
+        const ship = new Ship(2);
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    })
 })
