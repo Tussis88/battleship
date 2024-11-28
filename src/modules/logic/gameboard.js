f587821679ced07325757b748603f58455c01292
Andrea2 <andreab88@hotmail.it>
2024-11-28 17:59:20 +0100

    fatto placeShip()

--- src/modules/logic/gameboard.js
+++ src/modules/logic/gameboard.js
@@ -13,6 +13,14 @@ class Cell {
         return this.#isHit;
     }
 
+    set setShip(ship) {
+        this.#ship = ship;
+    }
+
+    get getShip() {
+        return this.#ship;
+    }
+
     hasShip() {
         return this.#ship != null;
     }
@@ -29,15 +37,51 @@ class Cell {
 class Gameboard {
     #cols = 10;
     #rows = 10;
-    #board;
+    #board = [];
+    #shipArray = [];
 
     constructor() {
-
+        this.resetBoard();
     }
 
     resetBoard() {
+        for (let i = 0; i < this.#rows; i++) {
+            this.#board[i] = [];
+            for (let j = 0; j < this.#cols; j++) {
+                this.#board[i].push(new Cell());
+            }
+        }
+    }
 
+    get getBoard() {
+        return this.#board;
+    }
+
+    placeShip(x, y, ship, isHorizontal) {
+        if (!this.#checkCoordvalue(x) || !this.#checkCoordvalue(y)) throw new Error("invalid coordinates");
+
+        this.#shipArray.push(ship);
+
+        let counter = ship.length;
+        if (isHorizontal && this.#checkCoordvalue(x + ship.length)) {
+            while (counter > 0) {
+                this.#board[x][y].setShip = ship;
+                x++;
+                counter--;
+            }
+        } else if (this.#checkCoordvalue(y + ship.length)) {
+            while (counter > 0) {
+                this.#board[x][y].setShip = ship;
+                y++;
+                counter--;
+            }
+        }
+    }
+
+    #checkCoordvalue(x) {
+        if (x < 0 || x > this.#cols) return false;
+        return true;
     }
 }
 
-export { Gameboard }
+export { Gameboard, Cell }
--- test/logic/gambeboard.test.js
+++ test/logic/gambeboard.test.js
@@ -1,7 +1,36 @@
-import { Gameboard } from "../../src/modules/logic/gameboard";
+import { Cell, Gameboard } from "../../src/modules/logic/gameboard";
+import { Ship } from "../../src/modules/logic/ship";
 
 describe("Gameboard", () => {
     test("Gameboard defined", () => {
         expect(Gameboard).toBeDefined();
     });
+
+    const board = new Gameboard();
+
+    test("get initialized board", () => {
+        expect(board.getBoard.length).toBe(10);
+    })
+
+    test("board has cells", () => {
+        board.getBoard.forEach(row => {
+            row.forEach(cell => {
+                expect(cell).toBeInstanceOf(Cell);
+            })
+        })
+    })
+
+    test("false positive has Ship", () => {
+        expect(board.getBoard[9][9].hasShip()).toBe(false);
+    })
+
+    test("place Ship", () => {
+        const ship = new Ship(3);
+        board.placeShip(3, 2, ship, true);
+        expect(board.getBoard[3][2].hasShip()).toBe(true);
+        expect(board.getBoard[3][2].getShip.length).toBe(3);
+        expect(board.getBoard[4][2].getShip.length).toBe(3);
+        expect(board.getBoard[5][2].getShip.length).toBe(3);
+        expect(board.getBoard[6][2].hasShip()).toBe(false);
+    })
 })
