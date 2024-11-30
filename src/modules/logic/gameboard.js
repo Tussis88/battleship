dac214d50cd5fcdc8baab105f1cdcba7f52ae7cf
Tussis <andreab88@hotmail.it>
2024-11-30 14:51:26 +0100

    Fatto placeShip()
    fatta funzione per piazzare la nave e fatta funzione per #checkLegalPos() per valutare se tutte le posizioni sono legali.
    Bisogna fare un refactor do quest`ultima funzione come fatto per placeShip()

--- src/modules/logic/gameboard.js
+++ src/modules/logic/gameboard.js
@@ -1,87 +1,97 @@
 import { Ship } from "./ship";
 
 class Cell {
-    #isHit;
-    #ship;
+  #isHit;
+  #ship;
 
-    constructor() {
-        this.#isHit = false;
-        this.#ship = null;
-    }
+  constructor() {
+    this.#isHit = false;
+    this.#ship = null;
+  }
 
-    get isHit() {
-        return this.#isHit;
-    }
+  get isHit() {
+    return this.#isHit;
+  }
 
-    set setShip(ship) {
-        this.#ship = ship;
-    }
+  set setShip(ship) {
+    this.#ship = ship;
+  }
 
-    get getShip() {
-        return this.#ship;
-    }
+  get getShip() {
+    return this.#ship;
+  }
 
-    hasShip() {
-        return this.#ship != null;
-    }
+  hasShip() {
+    return this.#ship != null;
+  }
 
-    hittedCell() {
-        if (this.hasShip()) {
-            this.#ship.hit();
-        } else {
-            this.isHit = true;
-        }
+  hittedCell() {
+    if (this.hasShip()) {
+      this.#ship.hit();
+    } else {
+      this.isHit = true;
     }
+  }
 }
 
 class Gameboard {
-    #cols = 10;
-    #rows = 10;
-    #board = [];
-    #shipArray = [];
+  #cols = 10;
+  #rows = 10;
+  #board = [];
+  #shipArray = [];
 
-    constructor() {
-        this.resetBoard();
+  constructor() {
+    this.resetBoard();
+  }
+
+  resetBoard() {
+    for (let i = 0; i < this.#rows; i++) {
+      this.#board[i] = [];
+      for (let j = 0; j < this.#cols; j++) {
+        this.#board[i].push(new Cell());
+      }
     }
+  }
 
-    resetBoard() {
-        for (let i = 0; i < this.#rows; i++) {
-            this.#board[i] = [];
-            for (let j = 0; j < this.#cols; j++) {
-                this.#board[i].push(new Cell());
-            }
-        }
+  get getBoard() {
+    return this.#board;
+  }
+
+  placeShip(x, y, ship, isHorizontal) {
+    if (!this.#checkLegalPos(x, y, ship, isHorizontal)) throw new Error("invalid coordinates");
+
+    this.#shipArray.push(ship);
+
+    const incrementX = isHorizontal ? 1 : 0;
+    const incrementY = isHorizontal ? 0 : 1;
+
+    for (let i = 0; i < ship.length; i ++) {
+      const newX = x + i * incrementX;
+      const newY = y + i * incrementY;
+
+      this.#board[newX][newY].setShip = ship;
+    };
+  }
+
+  #checkLegalPos(x, y, ship, isHorizontal) {
+    let counter = ship.length;
+    if (isHorizontal) {
+      if (x < 0 || (x + ship.length) >= this.#cols) return false;
+      while (counter > 0) {
+        if (this.#board[x][y].hasShip()) return false;
+        x++;
+        counter--;
+      }
+    } else {
+      if (y < 0 || (y + ship.length) >= this.#rows) return false;
+      while (counter > 0) {
+        if (this.#board[x][y].hasShip()) return false;
+        y++;
+        counter--;
+      }
     }
-
-    get getBoard() {
-        return this.#board;
-    }
-
-    placeShip(x, y, ship, isHorizontal) {
-        if (!this.#checkCoordvalue(x) || !this.#checkCoordvalue(y)) throw new Error("invalid coordinates");
-
-        this.#shipArray.push(ship);
-
-        let counter = ship.length;
-        if (isHorizontal && this.#checkCoordvalue(x + ship.length)) {
-            while (counter > 0) {
-                this.#board[x][y].setShip = ship;
-                x++;
-                counter--;
-            }
-        } else if (this.#checkCoordvalue(y + ship.length)) {
-            while (counter > 0) {
-                this.#board[x][y].setShip = ship;
-                y++;
-                counter--;
-            }
-        }
-    }
-
-    #checkCoordvalue(x) {
-        if (x < 0 || x > this.#cols) return false;
-        return true;
-    }
+    return true;
+  }
 }
 
 export { Gameboard, Cell }
