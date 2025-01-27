699f2337b7b42d94008197fda3b3645d74f2f510
Andrea2 <andreab88@hotmail.it>
2025-01-27 17:01:22 +0100

    riscritto renderer

--- src/modules/dom/gameboardRenderer.js
+++ src/modules/dom/gameboardRenderer.js
@@ -1,38 +1,64 @@
-function gameboardRenderer(player) {
-    const board = player.board.getBoard;
+function gameboardRenderer(game) {
+    const playerBoard = game.getPlayer().board.getBoard;
+    const cpuBoard = game.getCpu().board.getBoard;
+
+    const boardPlayerDiv = document.createElement("div");
+    boardPlayerDiv.classList.add("board");
+
+    const boardCpuDiv = document.createElement("div");
+    boardCpuDiv.classList.add("board");
+
     const boardDiv = document.createElement("div");
-    boardDiv.classList.add("board");
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
+    boardDiv.appendChild(boardPlayerDiv);
+    boardDiv.appendChild(boardCpuDiv);
+
+
+    for (let i = 0; i < playerBoard.length; i++) {
+        for (let j = 0; j < playerBoard[i].length; j++) {
             const cell = document.createElement("div");
+
             cell.classList.add("cell");
-            boardDiv.appendChild(cell);
-            if (player.isPlayer) {
-                if (board[i][j].hasShip() && board[i][j].isHit) {
-                    cell.classList.add("hit");
-                }
-                else if (board[i][j].hasShip()) {
-                    cell.classList.add("ship");
-                }
-                else if (board[i][j].isHit) {
-                    cell.classList.add("miss");
-                } else {
-                    cell.classList.add("water")
-                }
+            boardPlayerDiv.appendChild(cell);
+            if (playerBoard[i][j].hasShip() && playerBoard[i][j].isHit) {
+                cell.classList.add("hit");
+            }
+            else if (playerBoard[i][j].hasShip()) {
+                cell.classList.add("ship");
+            }
+            else if (playerBoard[i][j].isHit) {
+                cell.classList.add("miss");
             } else {
-                if (board[i][j].isHit) {
-                    if (board[i][j].hasShip()) {
-                        cell.classList.add("hit");
-                    } else {
-                        cell.classList.add("miss");
-                    }
-                } else {
-                    cell.classList.add("water");
-                }
+                cell.classList.add("water")
             }
         }
     }
+
+
+    for (let i = 0; i < cpuBoard.length; i++) {
+        for (let j = 0; j < cpuBoard[i].length; j++) {
+            const cellButton = document.createElement("button");
+            cellButton.classList.add("cell")
+            boardCpuDiv.appendChild(cellButton);
+            cellButton.addEventListener("click", () => {
+                game.playRound(i, j);
+                gameboardRenderer(game);
+            })
+            if (cpuBoard[i][j].hasShip() && cpuBoard[i][j].isHit) {
+                cellButton.classList.add("hit");
+            }
+            // else if (cpuBoard[i][j].hasShip()) {
+            //     cell.classList.add("ship");
+            // }
+            else if (cpuBoard[i][j].isHit) {
+                cellButton.classList.add("miss");
+            } else {
+                cellButton.classList.add("water")
+            }
+        }
+    }
+
     return boardDiv;
 }
+
 
 export { gameboardRenderer }
--- src/modules/dom/mainPage.js
+++ src/modules/dom/mainPage.js
@@ -3,12 +3,10 @@ import { gameboardRenderer } from "./gameboardRenderer";
 
 function mainPage() {
     const game = dummyGame();
-    const playerBoard = gameboardRenderer(game.getPlayer());
-    const cpuBoard = gameboardRenderer(game.getCpu());
+    const boards = gameboardRenderer(game);
 
     const main = document.createElement("main");
-    main.appendChild(playerBoard);
-    main.appendChild(cpuBoard);
+    main.appendChild(boards);
     return main;
 }
 
